921b0cb8035414bc7e47a9beb9089a4b48bc5c1e
Olivier Scalais <olivier@codify.be>
2019-10-29 09:01:51 +0100

    Create empty pod

---
+++ Example/Pods/Target Support Files/Workmanager/Workmanager-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Workmanager : NSObject
+@end
+@implementation PodsDummy_Workmanager
+@end
---
+++ Example/Workmanager/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Workmanager
+//
+//  Created by Olivier Scalais on 10/29/2019.
+//  Copyright (c) 2019 Olivier Scalais. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
